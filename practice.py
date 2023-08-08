42b6accfc6e57f176666e314a90b2b9d593fea85
AropetJ <aropetjoel32@gmail.com>
2023-08-08 22:30:29 +0300

    Completed the fourth task

--- practice.py
+++ practice.py
@@ -11,14 +11,19 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         """Defines the class object initialisee
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            if '__class__' in kwargs:
+                kwargs.pop('__class__')
+            if 'created_at' in kwargs:
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if 'updated_at' in kwargs:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            self.__dict__.update(kwargs)
 
-        for key, value in kwargs.items():
-            if key == 'created_at' or key == 'updated_at':
-                value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-            setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
@@ -27,7 +32,7 @@ class BaseModel():
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of
+        """Returns a dictionary containing all keys/values of __dict__ of
            the instance
 
         Returns:
